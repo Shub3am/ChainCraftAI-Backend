d8301c63f1640ba6e68c72727f209234b6c811f1
shub3am <shubhamvishwakarma0604@gmail.com>
2025-01-26 04:58:26 +0530

    Backend Finished, Flask Server Added

--- indexer.py
+++ indexer.py
@@ -5,6 +5,8 @@ from generateEmbedding import embeddings
 from vectorDB import vectorStore
 from vectorDB import persistClient
 from langchain_ollama import OllamaLLM
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
 
 
 def indexer():
@@ -35,12 +37,13 @@ def getQuery(query):
             return joinPrompts
 
 
-def generateQuery(query, similarity_search):   
-        LLM = OllamaLLM(model="qwen2.5-coder:7b")
-        prompt = LLM.invoke("generate a dapp using solidity")
-        print(prompt)
+def generateQuery(query,):   
+        llm = OllamaLLM(model="tinyllama")
+        LLM = OllamaLLM(model="qwen2.5-coder:3b")
+        get_similarity_search_query = llm.invoke("Generate Queries for the documentation of Solidity for the project: " + query)
+        RAG_data = getQuery(get_similarity_search_query)
+        dapp_prompt_template = PromptTemplate.from_template("You have to build a complete dapp using solidity for the {topic} \n Here are the details from Solidity's Documentation: {docs} \n Seperate Each File in Markdown")
+        promptGenerated = dapp_prompt_template.invoke({"topic": query, "docs": RAG_data})
+        return promptGenerated
         
- 
-
-generateQuery("ge", "")
   
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask
-from indexer import indexer, getQuery
+from flask import Flask, request, jsonify
+from langchain_groq import ChatGroq
+
+from indexer import indexer, getQuery, generateQuery
 app = Flask(__name__)
 indexer()
 @app.route("/")
 def hello_world():
-    l = getQuery("smart contracts")
-    print(l)
+  
     return "<p>Hello, World!</p>"
+
+@app.post("/generateQuery")
+def generate():
+    request_body = request.get_json()
+    print(request_body)
+    if ('prompt' in request_body):
+        results = generateQuery(request_body['prompt'])
+        return jsonify(results.json())
+    else:
+        return "Query Missing"
+    
+@app.post("/generateCode")
+def generateCode():
+    request_body = request.get_json()
+    print(request_body)
+    if ('prompt' in request_body):
+        LLM = ChatGroq(model="llama-3.3-70b-versatile",api_key="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")
+        results = LLM.invoke(request_body['prompt'] + 'use JSON')
+        print(results.json(), "results")
+        return jsonify(results.to_json())
+    else:
+        return "Query Missing"
+    
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, debug=True)
